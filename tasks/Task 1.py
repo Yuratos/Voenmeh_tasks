4d144c961a26256bdb771af6aeb52d720318578e
Yuratos <mdearse@gmail.com>
2023-09-12 20:09:05 +0300

    fixed mistakes

--- tasks/Task 1.py
+++ tasks/Task 1.py
@@ -1,15 +1,16 @@
-print(f"этот алгоритм вычисляет значение по функции: x*y*z*a^(1/2)\n"
+print(f"этот алгоритм вычисляет значение по функции: x*y*z*a^(u)\n"
       f"                                           ----------------\n"
-      f"                                           |b|*c*(d)^(1/3)")
+      f"                                           |b|*c*(d)^(e)")
 
-x = int(input(f'введите x: '))
-y = int(input(f'введите y: '))
-z = int(input(f'введите z: '))
-a = int(input(f'введите a: '))
-b = int(input(f'введите b: '))
-c = int(input(f'введите c: '))
+x = float(input(f'введите x: '))
+y = float(input(f'введите y: '))
+z = float(input(f'введите z: '))
+a = float(input(f'введите a: '))
+b = float(input(f'введите b: '))
+c = float(input(f'введите c: '))
 d = float(input(f'введите d: '))
-
-f = x * y * z * a ** 0.5
-F = abs(c)*c*d**1/3
+e = float(input(f'введите e: '))
+u = float(input(f'введите u: '))
+f = x * y * z * a ** u
+F = abs(b)*c*d**e
 print(f'результат: {f/F}')
